55542d1c91b3b153ad5c69dbff9cdaeaa331c77b
Sergei Khramtsov <vocmarh1989@gmail.com>
2022-01-31 01:43:14 +0500

    Add project files.

---
+++ WpfApp19/Models/Circuit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp19.Models
+{
+    class Circuit
+    {
+        public static int Add(int a, int b) => (int)(2 * Math.PI * a);
+    }
+}
---
+++ WpfApp19/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,66 @@
+﻿using WpfApp19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private int number1;
+        public int Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        private int number2;
+        public int Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+        private int number3;
+        public int Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecuted(object p)
+        {
+            Number3 = Circuit.Add(Number1, Number2);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1 != 0 || Number2 != 0)
+                return true;
+            else return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecuted, CanAddCommandExecuted);
+        }
+    }
+}
